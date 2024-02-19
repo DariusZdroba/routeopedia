55bf9390aa7491dc0f135f03c312b1a710bb0ac6
Darius <dariuszdroba@gmail.com>
2024-02-19 22:42:54 +0200

    initial routing setup

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Header from "./Components/Layout/Header";
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./Components/Pages/Home";
+import About from "./Components/Pages/About";
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<Header />);
+root.render(
+  <BrowserRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />}></Route>
+      <Route path="/about" element={<About />}></Route>
+    </Routes>
+  </BrowserRouter>
+);
